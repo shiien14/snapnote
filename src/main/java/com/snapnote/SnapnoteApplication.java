8d811d12149e07103a6e669e125c807ab521aa9d
shiien <shieunmo@gmail.com>
2025-04-26 18:21:34 +0900

    Add SApring Boot initial setup with .gitignore and application.yml

---
+++ src/main/java/com/snapnote/SnapnoteApplication.java
@@ -0,0 +1,13 @@
+package com.snapnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snapnote/SnapnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

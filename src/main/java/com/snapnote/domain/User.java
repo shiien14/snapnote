c66884c9329323d99ce3b637c882a4ece0346bb5
shiien <shieunmo@gmail.com>
2025-06-08 18:57:20 +0900

    feat: implement folder creation and list retrieval API

--- src/main/java/com/snapnote/domain/User.java
+++ src/main/java/com/snapnote/domain/User.java
@@ -13,6 +13,7 @@ public class User {
     private Long id;
 
     private String email;
+    private String name;
     private String password;
     private String nickname;
     private LocalDateTime createdAt;
